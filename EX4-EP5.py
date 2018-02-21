0adfac2a0830e49fde82a617ac0f7d0773b71c35
Unknown <G00364737@gmit.ie>
2018-02-21 19:29:32 +0000

    Exercise 4 - Euler Project 5 Solution

---
+++ EX4-EP5.py
@@ -0,0 +1,36 @@
+#  P5.f90
+
+#######################################################
+#  PROGRAM: P5
+#
+#  PURPOSE: Euler Project Problem 5 - Smallest multiple
+#
+#  PROBLEM STATEMENT:
+#
+#  2520 is the smallest number that can be divided by
+#  each of the numbers from 1 to 10 without any remainder.
+#
+#  What is the smallest positive number that is evenly
+#  divisible by all of the numbers from 1 to 20?
+#
+#######################################################
+
+num = 0
+divisor = [False] * 10
+
+while True:
+
+    num += 2520
+    divisor = [False for item in divisor]
+
+    for i in range(11, 21):
+        if num % i == 0:
+            divisor[i-11] = True
+        else:
+            break
+
+    if all(divisor):
+        break
+
+print('Smallest positive number is: ' + str(num))
+
