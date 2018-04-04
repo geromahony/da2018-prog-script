53ee7db239b1918c7328d4a944323752ddb73603
Unknown <G00364737@gmit.ie>
2018-04-04 13:35:13 +0100

    Exercise 5 - Iris Data set - initial commit - requirements in comments

---
+++ EX5-Iris-data-formatting.py
@@ -0,0 +1,9 @@
+# Ger O'Mahony
+
+# Exercise 5 Requirements:
+#
+# Write a Python script that reads the Iris data set in 
+# and prints the four numerical values on each row in a nice format. 
+# That is, on the screen should be printed the petal length, 
+# petal width, sepal length and sepal width, and these values should 
+# have the decimal places aligned, with a space between the columns.
