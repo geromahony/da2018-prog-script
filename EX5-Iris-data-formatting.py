cc4bb83c0ac9528b6aa2c5abad9beae3bf87ef70
Unknown <G00364737@gmit.ie>
2018-04-04 16:03:46 +0100

    EX5 - file open
    Open iris data file which is in IrisData folder in repo using relative path

--- EX5-Iris-data-formatting.py
+++ EX5-Iris-data-formatting.py
@@ -7,3 +7,10 @@
 # That is, on the screen should be printed the petal length, 
 # petal width, sepal length and sepal width, and these values should 
 # have the decimal places aligned, with a space between the columns.
+
+# read file in IrisData folder which is relative to this
+# file in repo (./)
+with open('./IrisData/iris.data.txt', 'r') as data_in:
+    for line in data_in:
+        print(line)
+
