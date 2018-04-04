e7b042ac1180ffe8bf7fc0e9ad87520d626e75e2
Unknown <G00364737@gmit.ie>
2018-04-04 20:41:37 +0100

    EX6 - Define Function
    Define function and input variable and check if it's a positive integer

--- EX6-Factorial-Function.py
+++ EX6-Factorial-Function.py
@@ -9,3 +9,11 @@
 # 5 is 5x4x3x2x1 which equals 120. You should, in your script, test the
 # function by calling it with the values 5, 7, and 10.
 # ######################################################################
+
+def factorial(num_in):
+    # check for positive integer first
+    if num_in > 0:
+        # calculate the factorial of num_in
+
+    else:
+        print('Please enter a positive integer')
