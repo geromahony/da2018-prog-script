8b464e5393a96e878713b9d157218c59ff3eb374
Unknown <G00364737@gmit.ie>
2018-04-04 16:45:10 +0100

    EX5 Print formatted data
    Print the formatted data. It's read as a string so just tidyup and allign

--- EX5-Iris-data-formatting.py
+++ EX5-Iris-data-formatting.py
@@ -11,6 +11,19 @@
 # read file in IrisData folder which is relative to this
 # file in repo (./)
 with open('./IrisData/iris.data.txt', 'r') as data_in:
+    # Print a header to show which column is which
+    print('|      Petal     |      Sepal     |')
+    print('| Length | Width | Length | Width |')
+    # Loop over each line in the data
     for line in data_in:
-        print(line)
+        # Remove the '/n' newline special character
+        # before splitting
+        raw_line = line.strip()
+
+        # Split data by comma
+        raw_line = raw_line.split(',')
+
+        # Print data using format descriptors.
+        # Data is read as a string already so just format that by aligning..
+        print('{:<3} {:<3} {:<3} {:<3}'.format(raw_line[0], raw_line[1], raw_line[2], raw_line[3]))
 
