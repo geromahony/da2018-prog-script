fcfb4dc2231be7aa45f7ecc4461b872c91070403
Unknown <G00364737@gmit.ie>
2018-04-08 20:05:14 +0100

    EX5 - Add reference for Iris data set
    Added download link for Iris data set

--- EX5-Iris-data-formatting.py
+++ EX5-Iris-data-formatting.py
@@ -2,15 +2,16 @@
 
 # Exercise 5 Requirements:
 # ##################################################################
-# Write a Python script that reads the Iris data set in 
+# Write a Python script that reads the Iris data set in
 # and prints the four numerical values on each row in a nice format. 
 # That is, on the screen should be printed the petal length, 
 # petal width, sepal length and sepal width, and these values should 
 # have the decimal places aligned, with a space between the columns.
 # ##################################################################
 
-# read file in IrisData folder which is relative to this
+# Read Iris Data[1] file in IrisData folder which is relative to this
 # file in repo (./)
+
 with open('./IrisData/iris.data.txt', 'r') as data_in:
     # Print a header to show which column is which
     print('===================================')
@@ -36,10 +37,11 @@ with open('./IrisData/iris.data.txt', 'r') as data_in:
         print('{:<3} {:<3} {:<3} {:<3}'.format(raw_line[0], raw_line[1], raw_line[2], raw_line[3]))
 
         # This is a bonus feature line!! Comment out the 'print' line above and uncomment this line
-        # to align the data with the column headers in a nice table like format. [1]
+        # to align the data with the column headers in a nice table like format. [2]
         # print('|{:^8}|{:^7}|{:^8}|{:^7}|'.format(raw_line[0], raw_line[1], raw_line[2], raw_line[3]))
 
 print('===================================')
 
 # References:
-# [1] https://docs.python.org/3.2/library/string.html#formatspec
+# [1] Iris Data Set - Downloaded from: https://archive.ics.uci.edu/ml/machine-learning-databases/iris/
+# [2] https://docs.python.org/3.2/library/string.html#formatspec
