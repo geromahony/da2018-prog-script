c56a1daae64052984799848d794ce39b79f2e61d
Unknown <G00364737@gmit.ie>
2018-04-08 18:29:41 +0100

    EX1-2 -Format improvements

--- EX-1-2.py
+++ EX-1-2.py
@@ -23,18 +23,19 @@
 
 
 def fib(n):
-  """This function returns the nth Fibonacci number."""
-  i = 0
-  j = 1
-  n = n - 1
-
-  while n >= 0:
-    i, j = j, i + j
+    """This function returns the nth Fibonacci number."""
+    i = 0
+    j = 1
     n = n - 1
-  
-  return i
 
-name = "OMahony"
+    while n >= 0:
+        i, j = j, i + j
+        n = n - 1
+  
+    return i
+
+
+name = 'OMahony'
 first = name[0]
 last = name[-1]
 firstno = ord(first)
@@ -42,8 +43,8 @@ lastno = ord(last)
 x = firstno + lastno
 
 ans = fib(x)
-print("My surname is", name)
-print("The first letter", first, "is number", firstno)
-print("The last letter", last, "is number", lastno)
-print("Fibonacci number", x, "is", ans)
+print('My surname is', name)
+print('The first letter', first, 'is number', firstno)
+print('The last letter', last, 'is number', lastno)
+print('Fibonacci number', x, 'is', ans)
 
